253fe663aa64621457712c355407988b3e2ea7c5
Dani <gdschanu.hieund1601@gmail.com>
2024-10-20 00:28:01 +0700

    add

--- Diagram/python_scripts/use_case.py
+++ Diagram/python_scripts/use_case.py
@@ -1,5 +1,13 @@
+import os
 import graphviz
 from graphviz import Digraph
+
+# Directory to store the result
+output_dir = 'result'
+
+# Create the directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 # Initialize the Digraph
 dot = Digraph('UseCaseDiagram', format='png')
@@ -51,5 +59,14 @@ dot.edge('Actor3', 'UC7')
 dot.edge('Actor3', 'UC8')
 dot.edge('Actor3', 'UC9')
 
-# Render the diagram to a file
-dot.render('use_case_diagram', view=True)
+# Specify the output file path inside the result folder
+output_file_path = os.path.join('F:/inventory_management-1/Diagram/result', 'use_case_diagram')
+
+# Render the diagram to the result folder
+dot.render(output_file_path, view=False)
+
+# Open and read the file if needed
+with open(f'{output_file_path}.png', 'rb') as file:
+    result = file.read()
+
+# The diagram is now stored in the 'result' folder as 'use_case_diagram.png'
